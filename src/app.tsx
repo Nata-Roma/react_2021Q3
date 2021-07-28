7ba5f20fea2871b10c293176d51c94b4d2464cb2
Natalia Romashko <sharkellgm@gmail>
2021-07-28 14:12:39 +0300

    feat: add lint plugin to webpack

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 import './app.css';
 import './styles.css';
 import Card from './components/card/card';
 import cardInfo from './utilities/configCard';
 
 const App = () => {
+  let cardInfoView:ReactElement | Array<ReactElement> = <div className="cards_noInfo">No card info</div>;
+
+  if(cardInfo.length) {
+    cardInfoView = cardInfo.map((card => (<Card {...card} key={card.id} />)))
+  }
   return (
     <div className="cards_wrapper">
-      {!cardInfo.length ? ( <div className="cards_noInfo">No card info</div>) : cardInfo.map((card => (
-        <Card {...card} key={card.id} />
-      )))}
+      {cardInfoView}
     </div>
   );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyPlugin = require('copy-webpack-plugin');
+const ESLintWebpackPlugin = require('eslint-webpack-plugin');
 
 const isProduction = process.env.NODE_ENV == "production";
 
@@ -33,7 +34,10 @@ const config = {
         },
       ],
     }),
-
+    new ESLintWebpackPlugin({
+      failOnError : true,
+  }),
+  
     // Add your plugins here
     // Learn more about plugins from https://webpack.js.org/configuration/plugins/
   ],
