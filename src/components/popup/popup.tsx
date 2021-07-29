bdb6eb17979ec913bca7616eb65d18ea17f0a529
Natalia Romashko <sharkellgm@gmail>
2021-07-29 09:33:08 +0300

    fix: refactor for lint errors

--- src/components/popup/popup.tsx
+++ src/components/popup/popup.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import './modal-popup.css';
 
-const Popup = () => {
-  return (
-    <div className="modal_outter">
-      <div className="modal_popup">Data has been saved successfully!</div>
-    </div>
-  );
-};
+const Popup = (): JSX.Element => (
+  <div className="modal_outter">
+    <div className="modal_popup">Data has been saved successfully!</div>
+  </div>
+);
 
 export default Popup;
