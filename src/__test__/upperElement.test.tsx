a9c225ea8d3816a9f03d253aeaf34279e340eb56
Natalia Romashko <sharkellgm@gmail>
2021-08-21 19:43:57 +0300

    feat: add testing 404 page

--- src/__test__/upperElement.test.tsx
+++ src/__test__/upperElement.test.tsx
@@ -2,7 +2,8 @@ import React from "react"
 import UpperElement from "../components/upperElement"
 
 jest.mock('../components/pages/home-page', () => () => <div>Home</div>);
-jest.mock('../components//pages/aboutPage', () => () => <div>About</div>);
+jest.mock('../components/pages/aboutPage', () => () => <div>About</div>);
+jest.mock('../components/pages/page404', () => () => <div>404</div>);
 
 describe('routing', () => {
   it('renders Home page at "/"', () => {
@@ -12,5 +13,10 @@ describe('routing', () => {
   it('renders About page at "/about"', () => {
     const {container} = renderWithRouter(() => <UpperElement />, "/about")
     expect(container.innerHTML).toMatch('About');
+  })
+
+  it('renders 404 page at "/aboutryt"', () => {
+    const {container} = renderWithRouter(() => <UpperElement />, "/aboutryt")
+    expect(container.innerHTML).toMatch('404');
   })
 })
